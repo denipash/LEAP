f88ccc17cdb142c10cb694679f2bdc86d5a848c0
denipash <52679116+denipash@users.noreply.github.com>
2021-07-19 15:39:48 -0400

    Create modules_finished.py

---
+++ modules_finished.py
@@ -0,0 +1,15 @@
+# importing the math module, which contains features for working with mathematics
+import math
+
+# the math module contains lots of pre-built functions
+print("The square root of 16 is", math.sqrt(16))
+
+# in addition to functions, some modules contain useful constants 
+print("Pi is:", math.pi)
+
+# trying a math function not in the examples given below:
+angleInDegree = 90
+angleInRadian = math.radians(angleInDegree)
+print('sin(x) is :' math.sin(angleInRadian))
+print('cos(x) is :' math.cos(angleInRadian))
+print('tan(x) is :' math.tan(angleInRadian))
